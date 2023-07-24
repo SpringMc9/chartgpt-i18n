90fa093e6bef40beaea19e6d389a6207d2bd71bf
1946697229 <1946697229@qq.com>
2023-07-24 15:45:12 +0800

    style:el-popover高度修改

--- src/services/translatefiles.js
+++ src/services/translatefiles.js
@@ -18,10 +18,7 @@ export async function translateAndExportFiles(req) {
     const messages = [
       {
         role: "system",
-        // content: `You are a helpful assistant that translates a i18n locale array content to ${targetLang}.
-        //       It's a array structure, contains many strings, translate each of them and make a new array of translated strings.
-        //       Consider all the string as a context to make better translation.\n`,
-        content: `You are a helpful assistant that translates a i18n locale key-value objects to ${targetLang}.
+        content: `You are a helpful assistant that translates a i18n locale key-value objects to ${targetLang[i]}.
             It's a key-value objects, contains many key-value values, translate each of them and make a new array of translated strings.
             Consider all the string as a context to make better translation.\n`,
       },
@@ -29,15 +26,12 @@ export async function translateAndExportFiles(req) {
     if (typeof extraPrompt === "string" && extraPrompt.length > 0) {
       messages.push({
         role: "user",
-        // content: `Other tips for translation: ${extraPrompt}\n
-        //       Translate this array: \n\n\n`,
         content: `Other tips for translation: ${extraPrompt}\n  
             Translate this key-value object: \n\n\n`,
       });
     } else {
       messages.push({
         role: "user",
-        // content: `Translate this array: \n\n\n`,
         content: `Translate this key-value object: \n\n\n`,
       });
     }
@@ -49,12 +43,10 @@ export async function translateAndExportFiles(req) {
     const tasks = [];
     const CHUNK_SIZE = 60;
     let chunk = [];
-    // let chunkSize = 0;
+    console.log(requireTranslation);
     for (let i = 0; i < requireTranslation.length; i++) {
-      // chunk.push(requireTranslation[i][1]);
       chunk.push(requireTranslation[i]);
-      // chunkSize += requireTranslation[i][1].length;
-      // if (chunkSize >= CHUNK_SIZE) {
+      console.log(chunk);
       if (chunk.length >= CHUNK_SIZE) {
         const freezeChunk = [...chunk];
         const finishedTask = await createChatCompletion({
@@ -67,6 +59,7 @@ export async function translateAndExportFiles(req) {
           ],
         })
           .then((completion) => {
+            console.log(completion);
             return matchJSON(completion);
           })
           .then((raw) => {
@@ -121,6 +114,7 @@ export async function translateAndExportFiles(req) {
     const translated = (await Promise.all(tasks)).flatMap((t) => t);
     const nextPairs = translated.concat(noTranslation);
     const result = buildJsonByPairs(nextPairs);
+    console.log(result);
     const data = prettierJson(result);
     translations.push(data);
   }
